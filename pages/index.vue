77ed17f391e5d2129aa0da6c77892688778e0f1e
b0ul17 <b0ul17@hackthebox.eu>
2024-08-10 02:50:23 -0700

    chore: Refactor index.vue component for improved readability and maintainability

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-2xl min-h-screen flex flex-col justify-center">
     <div
-      class=" mx-auto max-w-[800px] p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100 bg-neutral-50 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 rounded-lg  ring-1 ring-gray-200 shadow"
+      class=" mx-auto p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100 bg-neutral-50 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 rounded-lg  ring-1 ring-gray-200 shadow"
     >
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        {{ jokeArray[0] }}
-      </h5>
-      <p class="font-normal text-gray-700 dark:text-gray-400 text-center">
-        {{ jokeArray[1] }}
+      <p
+        v-for="(line, index) in jokeArray"
+        :key="index"
+        class="text-2xl font-bold text-gray-900 dark:text-white text-balance"
+      >
+        {{ line }}
       </p>
     </div>
   </div>
